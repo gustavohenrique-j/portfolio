830d51926c89672bdda41c7a32617cbf9ebe50c8
Gustavo Romano <gustavohenrikedev@gmail.com>
2025-11-29 14:26:47 -0300

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/portfolio/",
+  base: "./",         // caminhos relativos — recomendado para gh-pages
   plugins: [react()],
 });
